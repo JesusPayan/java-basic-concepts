a2dfae6b8b5603443d01879ccb36141e80dbb54b
Jesús Armando Hernández Payán <55152025+JesusPayan@users.noreply.github.com>
2022-10-17 11:12:52 -0600

    Add files via upload

---
+++ src/topics/collections/CountingCharacters.java
@@ -0,0 +1,38 @@
+/**
+
+*  Ejercicio Entrevistas JAVA
+
+*
+
+*  Obtener el numero de repeticiones de cada letra en la cadena e imprimir la lista ordenada por el total de repeticiones
+
+*
+
+*   Solo letras y usando Java 8
+
+*
+
+*   "aafldkerogkvmcndjwo3qpwoeorroepwpspxlzklokokoedwsqaascvbnorewsazxpcmvnbjgkdlwp2098ttkkel" +
+
+*   "jgjfhfhgjfkdlepr09wqotqqdsdfwhjcvxzqazyzsxewepoti?yuiooplknvcftrewqweyuioplkhjklkjhgfdsw " +
+
+*   "qazx093fgbnmopeopeoepo3l4kfjfhghgkikgiaaawsdcvbmloioqwedghghfiopwpeorioaaplmxxcvdeswwoes" +
+
+*   "ookkkjhg!aedwqpolm?nbvgtrfdcxftghbnjmlkoiuytrerer76dd9vbpoldslkeorkfokkroi?ofdw" ;
+
+*
+
+*/
+public  class CountingCharacters {
+
+    public static void main(String[] args) {
+        String cadena = "aafldkerogkvmcndjwo3qpwoeorroepwpspxlzklokokoedwsqaascvbnorewsazxpcmvnbjgkdlwp2098ttkkel" +
+
+                "jgjfhfhgjfkdlepr09wqotqqdsdfwhjcvxzqazyzsxewepoti?yuiooplknvcftrewqweyuioplkhjklkjhgfdsw " +
+
+                "qazx093fgbnmopeopeoepo3l4kfjfhghgkikgiaaawsdcvbmloioqwedghghfiopwpeorioaaplmxxcvdeswwoes" +
+                "ookkkjhg!aedwqpolm?nbvgtrfdcxftghbnjmlkoiuytrerer76dd9vbpoldslkeorkfokkroi?ofdw";
+        System.out.println(cadena);
+    }
+
+}
