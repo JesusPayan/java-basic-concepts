6b6f84b53f28067a94407eccc2cf649283372d47
Marcos Cauich <marcos.18070072@itsmotul.edu.mx>
2022-11-24 11:05:58 -0600

    Ejercicios Array list y Linked List

--- src/collections/List/VectorExample.java
+++ src/collections/List/VectorExample.java
@@ -1,6 +1,16 @@
 package collections.List;
 
+import java.util.Scanner;
+
 /*TODO:Create a LinkedList of Person and Dog in utilities packqge
  *  apply the methods add remove, removeAll, size to array*/
 public class VectorExample {
+    public static void main(String[] args) {
+        int cont = 0;
+        int limite;
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("¿Cuantos clientes desea aingresar?");
+
+    }
 }
